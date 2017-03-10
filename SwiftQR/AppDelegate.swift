72c0919a97bb76e4610fca9ed2f2eb14d101628f
maquannene <maquannene@gmail.com>
2017-03-10 20:06:54 +0800

    Initial Commit

---
+++ SwiftQR/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SwiftQR
+//
+//  Created by 马权 on 10/03/2017.
+//  Copyright © 2017 马权. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
